a64e1de56610a33251d5fa2b08d5bdaec350c7c2
James Martin <jamesmartin@github.com>
2016-09-05 14:21:54 +1000

    QUestions go to /question

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/', function (req, res) {
+app.post('/question', function (req, res) {
   res.type('json');
   res.set('Content-type', 'application/json');
   res.send({ text: "Hello" });
