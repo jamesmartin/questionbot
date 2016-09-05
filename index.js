fb24b73add700f96f65180bd7c4e1f8a5ea2fa8e
James Martin <jamesmartin@github.com>
2016-09-05 14:42:31 +1000

    Respond with slack flavored json body

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser')
 var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(logger('dev'));
 
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/question', function (req, res) {
+  var response = {
+    response_type: 'in_channel',
+    attachments: [
+      {
+        text: req.body.text
+      }
+    ]
+  }
+
   res.type('json');
   res.set('Content-type', 'application/json');
-  res.send({ text: "Hello" });
+  res.send(response);
 });
 
 app.listen(app.get('port'), function () {
