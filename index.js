172b8446db235dce9b88588004454bf47dc7af61
James Martin <jamesmartin@github.com>
2016-09-05 14:21:23 +1000

    POST...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
+app.post('/', function (req, res) {
   res.type('json');
   res.set('Content-type', 'application/json');
   res.send({ text: "Hello" });
