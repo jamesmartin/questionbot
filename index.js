6480290f47b2a8044c85d7db50d1286c646245cd
Andrew Carr <andrw.carr@gmail.com>
2016-09-06 13:12:03 +1000

    Actions as a seperate attachment

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.post('/question', function (req, res) {
       {
         footer: 'Asked by <@'+req.body.user_id+'|'+req.body.user_name + '>',
         title: req.body.text,
-        color: "#c41e4b",
+        color: "#c41e4b"
+      },
+      {
+        color: "#ffffff",
         actions: [
           {
             name: "answer",
